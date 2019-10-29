df081f8ff5c7da3cd623c4925939512bc2be2ad4
xKMarcus <45180209+xKMarcus@users.noreply.github.com>
2019-10-29 14:04:56 +0000

    My first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+# @Date:   2019-10-29T13:13:30+00:00
+# @Last modified time: 2019-10-29T13:40:55+00:00
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('helloworld', function () {
+    return '<h1>Hello World</h1>';
+});
+
+Route::get('marcus', function () {
+    return view('greeting');
+});
